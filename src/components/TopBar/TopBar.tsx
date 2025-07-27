a71e180525b48396405d9aceeadb62ec4678fd8b
Poujhit <poujhit.dev@gmail.com>
2025-07-27 15:14:59 +0530

    Refactor TopBar component: fix string concatenation for "Party" label and clean up deleteRoom API call URL formatting

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -390,7 +390,7 @@ export class TopBar extends React.Component<{
                       marginLeft: 'auto',
                     }}
                   >
-                    Party
+                    {' ' + 'Party'}
                   </div>
                 </div>
               </a>
